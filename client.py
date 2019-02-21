1aa3d17ce15beee939b7ad5ca6196aec75bac95c
ivanl <ilukomskiy@sbdagroup.com>
2019-02-21 21:09:08 +0300

    UDP protocol version

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import json
+import socket
+import struct
 import time
 from threading import Thread
 
-import urllib3
 from inputs import get_gamepad
 
 # Controller settings
@@ -12,26 +13,25 @@ LEFT_MAX = -32768
 RIGHT_MAX = 32767
 INPUT_CODE = 'ABS_RX'
 
-# Rest client settings
-REST_CLIENT_MIN_DELAY = 0.2
-API_URI = 'http://localhost:8000'
-HEADERS = {'Connection': 'close'}
-PATH = "{}/api/servo/y/velocity".format(API_URI)
-
-http = urllib3.PoolManager()
+# UDP client settings
+CLIENT_MIN_DELAY = 0.005
+SERVER_IP = 'localhost'
+SERVER_PORT = 5005
+LOWERING_COEFFICIENT = 0.3
 
 
 def position_to_percents(value):
     if LEFT_THRESHOLD < value < RIGHT_THRESHOLD:
         return 0
     if value < 0:
-        return - value * 100 / LEFT_MAX
-    return value * 100 / RIGHT_MAX
+        return - value * 100 * LOWERING_COEFFICIENT / LEFT_MAX
+    return value * 100 * LOWERING_COEFFICIENT / RIGHT_MAX
 
 
 class MzClient:
     currentValue = 0
     driverValue = 0
+    sock = None
 
     def events_handling_loop(self):
         while 1:
@@ -43,20 +43,20 @@ class MzClient:
 
     def rest_client_loop(self):
         while 1:
-            time.sleep(0.2)
+            time.sleep(CLIENT_MIN_DELAY)
             if self.driverValue == self.currentValue:
                 continue
+
             self.driverValue = self.currentValue
-            encoded_data = json.dumps({"value": self.driverValue}).encode('utf-8')
-            start = time.time()
-            r = http.request('PUT', PATH, body=encoded_data)
-            val = json.loads(r.data.decode('utf-8'))['value']
-            end = time.time()
-            print("Velocity updated: {0:.2f}, took {1:.4f} sec".format(val, (end - start)))
+            self.sock.sendto(struct.pack('bf', 1, self.driverValue), (SERVER_IP, SERVER_PORT))
+            print("Set to {}".format(self.driverValue))
+
 
     def startup(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         Thread(target=self.events_handling_loop).start()
         Thread(target=self.rest_client_loop()).start()
+
 
 
 client = MzClient()
