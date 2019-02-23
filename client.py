074775201c4560d0eea6cfcdb71672401fb97985
ivanl <ilukomskiy@sbdagroup.com>
2019-02-23 21:19:25 +0300

    UDP protocol version updated: timestamp added

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import socket
 import struct
 import time
 from threading import Thread
+current_millis = lambda: int(round(time.time() * 1000))
 
 from inputs import get_gamepad
 
@@ -14,18 +15,19 @@ RIGHT_MAX = 32767
 INPUT_CODE = 'ABS_RX'
 
 # UDP client settings
-CLIENT_MIN_DELAY = 0.005
-SERVER_IP = 'localhost'
+CLIENT_MIN_DELAY = 0.01
+SERVER_IP = '192.168.88.249'
 SERVER_PORT = 5005
 LOWERING_COEFFICIENT = 0.3
+SHIFT = -1.5
 
 
 def position_to_percents(value):
     if LEFT_THRESHOLD < value < RIGHT_THRESHOLD:
-        return 0
+        return SHIFT
     if value < 0:
-        return - value * 100 * LOWERING_COEFFICIENT / LEFT_MAX
-    return value * 100 * LOWERING_COEFFICIENT / RIGHT_MAX
+        return - value * 100 * LOWERING_COEFFICIENT / LEFT_MAX + SHIFT
+    return value * 100 * LOWERING_COEFFICIENT / RIGHT_MAX + SHIFT
 
 
 class MzClient:
@@ -48,7 +50,8 @@ class MzClient:
                 continue
 
             self.driverValue = self.currentValue
-            self.sock.sendto(struct.pack('bf', 1, self.driverValue), (SERVER_IP, SERVER_PORT))
+            self.sock.sendto(struct.pack('bf', 1, self.driverValue, current_millis()),
+                             (SERVER_IP, SERVER_PORT))
             print("Set to {}".format(self.driverValue))
 
 
