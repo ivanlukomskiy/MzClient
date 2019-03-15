73a1065dce1867d30d17f22927f9a6a1b7dcbe24
ivanl <ilukomskiy@sbdagroup.com>
2019-03-16 02:03:38 +0300

    Two channels support

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
-import json
 import socket
 import struct
 import time
 from threading import Thread
+
 current_millis = lambda: int(round(time.time() * 1000))
 
 from inputs import get_gamepad
@@ -12,7 +12,10 @@ LEFT_THRESHOLD = -3000
 RIGHT_THRESHOLD = 3000
 LEFT_MAX = -32768
 RIGHT_MAX = 32767
-INPUT_CODE = 'ABS_RX'
+X_INPUT_CODE = 'ABS_RX'
+Y_INPUT_CODE = 'ABS_RY'
+X_CHANNEL = 1
+Y_CHANNEL = 2
 
 # UDP client settings
 CLIENT_MIN_DELAY = 0.01
@@ -29,35 +32,39 @@ def position_to_percents(value):
 
 
 class MzClient:
-    currentValue = 0
-    driverValue = 0
+    current_x_value = 0
+    driver_x_value = 0
+    current_y_value = 0
+    driver_y_value = 0
     sock = None
 
     def events_handling_loop(self):
         while 1:
             events = get_gamepad()
             for event in events:
-                if event.code != INPUT_CODE:
-                    continue
-                self.currentValue = position_to_percents(event.state)
+                if event.code == X_INPUT_CODE:
+                    self.current_x_value = position_to_percents(event.state)
+                elif event.code == Y_INPUT_CODE:
+                    self.current_y_value = position_to_percents(event.state)
 
     def rest_client_loop(self):
         while 1:
             time.sleep(CLIENT_MIN_DELAY)
-            if self.driverValue == self.currentValue:
-                continue
-
-            self.driverValue = self.currentValue
-            self.sock.sendto(struct.pack('bfq', 1, self.driverValue, current_millis()),
-                             (SERVER_IP, SERVER_PORT))
-            print("Set to {}".format(self.driverValue))
-
+            if self.driver_x_value != self.current_x_value:
+                self.driver_x_value = self.current_x_value
+                self.sock.sendto(struct.pack('bfq', X_CHANNEL, self.driver_x_value, current_millis()),
+                                 (SERVER_IP, SERVER_PORT))
+                print("X set to {}".format(self.driver_x_value))
+            elif self.driver_y_value != self.current_y_value:
+                self.driver_y_value = self.current_y_value
+                self.sock.sendto(struct.pack('bfq', Y_CHANNEL, self.driver_y_value, current_millis()),
+                                 (SERVER_IP, SERVER_PORT))
+                print("Y set to {}".format(self.driver_y_value))
 
     def startup(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         Thread(target=self.events_handling_loop).start()
         Thread(target=self.rest_client_loop()).start()
-
 
 
 client = MzClient()
